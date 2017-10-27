0fa06e88753235cf231fcb121124df47dd36a001
David Robinson <David.Robinson@practiceweb.co.uk>
2017-10-27 10:10:29 +0100

    Changed deadlines to support either file upload or download from specified location

--- src/Deadlines/DeadlinesService.php
+++ src/Deadlines/DeadlinesService.php
@@ -28,6 +28,7 @@ class DeadlinesService extends ServiceAbstract
         //$this->addAction('et_builder_ready', 'registerDiviModules');
         $this->addAction('wp_enqueue_scripts', 'registerScripts');
         $this->addAction('admin_notices', 'showAdminNotices');
+        $this->addAction('admin_enqueue_scripts', 'registerAdminScripts');
     }
 
     /**
@@ -416,10 +417,21 @@ class DeadlinesService extends ServiceAbstract
     }
 
     /**
+     * Register javascript.
+     */
+    public function registerAdminScripts()
+    {
+        $cssPath = plugin_dir_url($this->pluginFile) . '/css/deadlines-admin.css';
+        wp_register_style('deadlines-admin', $cssPath);
+    }
+
+    /**
      * Page view for upload.
      */
     public function uploadPage()
     {
+        // Add our css
+        wp_enqueue_style('deadlines-admin');
         $vars = array();
         $this->renderTemplate('deadlines/upload', $vars);
     }
@@ -430,11 +442,30 @@ class DeadlinesService extends ServiceAbstract
     public function uploadPageSubmit()
     {
         // TODO verify
-        $uploaded = $_FILES['deadlinesfile'];
-        $fh = fopen($uploaded['tmp_name'], 'r');
+        $loadMethod = $_REQUEST['loadMethod'];
+        $handle = null;
+        switch ($loadMethod) {
+            case 'download':
+                $config = get_option('practiceweb-connectivity-config', array());
+                $apiKey = $config['apiKey'];
+                $url = $_REQUEST['url'];
+                $url = add_query_arg('apiKey', urlencode($apiKey), $url);
+                $response = wp_remote_get($url);
+                $body = wp_remote_retrieve_body($response);
+                // Hold the content a stream so that we can use one method to read.
+                $handle = fopen('php://temp', 'r+');
+                fwrite($handle, $body);
+                rewind($handle);
+                break;
+            case 'upload':
+                $uploaded = $_FILES['deadlinesfile'];
+                $handle = fopen($uploaded['tmp_name'], 'r');
+                break;
+        }
+
         // Assume that first line is header
-        $header = fgetcsv($fh);
-        while ($row = fgetcsv($fh)) {
+        $header = fgetcsv($handle);
+        while ($row = fgetcsv($handle)) {
             list($title, $uuid, $deadlineDate, $content, $teaser, $termsString, $guid) = $row;
             $terms = array_unique(array_map('trim', explode(',', $termsString)));
             $postInfo = array(
@@ -463,6 +494,7 @@ class DeadlinesService extends ServiceAbstract
                 wp_set_object_terms($postId, $terms, 'PracticeWEBContent');
             }
         }
+        fclose($handle);
         $messageKey = get_current_user_id() . '_pwdeadlines';
         set_transient($messageKey, 'Uploaded deadlines data.');
         wp_redirect(admin_url('admin.php?page=deadlines-upload'));
--- templates/deadlines/upload.php
+++ templates/deadlines/upload.php
@@ -1,8 +1,24 @@
+<h2>Setup your Practiceweb Deadlines Content</h2>
 <form method="POST" action="<?php echo esc_url(admin_url('admin-post.php')); ?>"  enctype="multipart/form-data">
     <?php wp_nonce_field('practiceweb_connectivity_deadlines_upload') ?>
+
+    <h3>Choose method:</h3>
+    <div>
+
     <fieldset>
-        <legend>Upload deadlines</legend>
-        <label>File: <input type="file" name="deadlinesfile"></label>
+        <label for="loadMethod-upload">Upload deadlines</label>
+        <input type="radio" name="loadMethod" value="upload" id="loadMethod-upload">
+        <div class="radio-reveal">
+            <label for="deadlinesfile">File: </label><input type="file" name="deadlinesfile">
+        </div>
+    </fieldset>
+
+    <fieldset>
+        <label for="loadMethod-download">Download deadlines</label>
+        <input type="radio" name="loadMethod" value="download" id="loadMethod-download">
+        <div class="radio-reveal">
+            <label for="url">Url: </label><input type="text" name="url">
+        </div>
     </fieldset>
     <input type="hidden" name="action" value="practiceweb_connectivity_deadlines_upload">
     <?php submit_button(); ?>
